53b25647db4baa7f93418096d16ab58eaba5e13a
James Dalton Baker <james.Baker59@stu.bmcc.cuny.edu>
2026-08-18 11:01:54 -0400

    Remove row-level trash action

--- CleanSlate_sidebar/src/components/EmailListItem.jsx
+++ CleanSlate_sidebar/src/components/EmailListItem.jsx
@@ -9,7 +9,6 @@ function EmailListItem({
     conversation,
     isSelected,
     onToggleSelected,
-    onTrash,
 }) {
     return (
         <li className="email-list__item">
@@ -51,23 +50,6 @@ function EmailListItem({
                     </p>
                 </details>
 
-                <button
-                    type="button"
-                    className="email-trash-btn"
-                    aria-label={`Move ${conversation.subject || "conversation"} to trash`}
-                    title="Move to trash"
-                    onClick={() => onTrash?.(conversation.id)}
-                >
-                    <svg
-                        viewBox="0 0 24 24"
-                        aria-hidden="true"
-                    >
-                        <path d="M4 7h16" />
-                        <path d="M9 7V4h6v3" />
-                        <path d="M6.5 7 8 20h8l1.5-13" />
-                        <path d="M10 11v5M14 11v5" />
-                    </svg>
-                </button>
             </div>
         </li>
     )
